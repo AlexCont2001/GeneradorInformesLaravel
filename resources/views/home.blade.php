ed61258b001f81e4fcd71dab34c1e4b852faf8d4
alexcontreras-developer <alexcontreras.developer@gmail.com>
2025-11-24 00:48:53 -0300

    Welcome Layout

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="alert alert-warning" role="alert">
-    A simple warning alert-check it out
-</div>
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
-});
 Route::get('/estudiantes', [EstudiantesController::class, 'index'])->name('estudiantes.index');
 Route::get('/estudiantes/crear', [EstudiantesController::class, 'crear'])->name('estudiantes.crear');
 Route::post('/estudiantes/guardar', [EstudiantesController::class, 'guardar'])->name('estudiantes.guardar');
